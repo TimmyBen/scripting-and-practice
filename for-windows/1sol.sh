02bbe6fc30a1fcd8caa2e0d37a8a6d925f74d2a0
Timi Adewumi <nucleuscaesar@yahoo.com>
2022-07-11 02:46:17 +0100

    new changes

--- for-windows/1sol.sh
+++ for-windows/1sol.sh
@@ -5,12 +5,31 @@ oIFS=$IFS
 IFS=","
 while read firstname lastname username 
 do
-    sudo useradd $firstname
-    if [[ ! $(cat /etc/group | grep engineering) ]];
+    if [[ $firstname = *"First"* ]] ||  [[ $lastname = *"Last"* ]] ||  [[ $username = *"Username"* ]] 
     then
-        sudo groupadd engineering
-    sudo usermod -d "/home/$firstusername" -aG engineering 
-    userpasswd=$RANDOM
-    echo -e "$userpasswd\n$userpasswd" | sudo passwd $firstname
-    echo "$firstname's password: $userpasswd" >> passwd.txt 
+        echo "Skipping this"
+        sleep 1
+        echo
+    elif [[ ! $(cat /etc/passwd | grep "$username") ]]
+    then
+        echo "Skipping again"
+    else 
+        sudo useradd "$username"
+        fi
+        if [[ ! $(cat /etc/group | grep engineering) ]];
+        then
+            sudo groupadd engineering
+            sudo usermod -d "/home/$username" -aG engineering $username
+            userpasswd=$RANDOM
+            echo -e "$userpasswd\n$userpasswd" | sudo passwd $username
+            echo "$username's password: $userpasswd" >> passwd.txt 
+        fi
+    fi
+    # echo "The First Name is: $firstname, Last name is: $lastname and Username is: $username"
+    # sleep 1
+    # date
+    # if [[ -z "$firstname" ||  -z "$lasttname" ||  -z "$username" ]]
+    # then 
+    #     exit 0
+    # fi
 done < ./sample.csv
